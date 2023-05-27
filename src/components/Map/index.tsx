910bcc49c4db4f026d39b4ec97fbda7afa5895d7
EduardoBezerraDev <profissional.edu.teixeira@gmail.com>
2023-05-27 13:29:32 -0300

    fix/ fix wrong path of map component

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
-import { styles } from '../style';
+import { styles } from './style';
 import {
   requestForegroundPermissionsAsync,
   getCurrentPositionAsync,
